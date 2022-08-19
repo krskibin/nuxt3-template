2ff63e1206f1f9e8f6b129dca413d57e89d9f2f6
Krystian (krskibin) <krskibin@gmail.com>
2022-08-20 00:31:22 +0200

    Disable no-undef rule (leave it for tsc) and add scss support

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   ],
   rules: {
     quotes: ['error', 'single'],
+    'no-undef': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <NuxtWelcome />
+    <div class="test-value">{{ value }}</div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const value = ref('');
+</script>
+
+<style lang="scss" scoped>
+.test-value {
+  cursor: pointer;
+  &:hover {
+    color: green;
+  }
+}
+</style>
